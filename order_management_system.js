997456bf8ec803f428e522e52764920f1a3c6c01
Kyle Thomas <kylethomas1@usf.edu>
2024-09-22 16:47:25 -0400

    Create placeOrder function

--- order_management_system.js
+++ order_management_system.js
@@ -26,9 +26,31 @@ let Inventory = [
 
 // Task 2: Create an Orders Array of Order Objects
 
-let Orders = []             // Array of Order Objects
+let Orders = [];            // Created orders array
 
+// Task 3: Create a Function to Place an Order
+let items = (name, quantityOrdered)
 
-
-
+function customerOrder(customerName, items) {  // customerOrder will include customerNmae, name, and quantity
+    const Order = {
+        customerName: customerName,
+        items: items    //Order includes CustomerName and items
+    }
 }
+
+function placeOrder(customerName, orderedItems) {
+    items.quantity = (items.quantity - items.quantityOrdered);  //Reduce quantity based on sales
+    if (items.quantity >= items.quantityOrdered) {
+         console.log("In Stock");
+    } else if  (items.quantity == 0) {
+         console.log("Error: Out of Stock");
+    }
+}                   
+
+const Order = {
+    customerName: customerName,
+    items: orderedItems,
+    status: "Pending"     //Added order to orders array with status "pending"
+}
+
+Orders.push(Order);  //Added the order to orders array
