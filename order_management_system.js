f0a18a934a90268369beb2322fcd22654cf672cb
Kyle Thomas <kylethomas1@usf.edu>
2024-09-21 20:36:34 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,25 @@
+// Task 1
+
+let Inventory = [
+    {
+        name: 'Jeans',
+        price: 50,
+        quantity: 100000,
+    }    
+    {
+        name: 'Jackets',
+        price: 65,
+        quantity: 100000,
+    }
+    {
+        name: 'Shirts',
+        price: 30,
+        quantity: 100000,
+    }
+    {
+        name: 'Socks',
+        price: 15,
+        quantity: 100000,
+    }
+]
+//array of objects
